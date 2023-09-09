3d0ad0dd97f483379f0ebf8c896e3001ab368b30
Wilder <w.cossio.r@gmail.com>
2023-09-08 20:14:35 -0400

    fixing the issue when playing more than one audio

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const soundBoxes = document.querySelectorAll('.btn');
 soundBoxes.forEach(soundBox => {
     let soundType = soundBox.textContent.toLocaleLowerCase();
     soundBox.addEventListener('click', () => {
-        let audio = document.querySelector(`#${soundType}`);
-        audio.play();
+        stoppingTheRestOfTheAudios();
+        let sound = document.querySelector(`#${soundType}`);
+        sound.play();
     });
 });
+
+function stoppingTheRestOfTheAudios() {
+    let allTheSounds = document.querySelectorAll('audio');
+    allTheSounds.forEach(sound => {
+        sound.pause();
+        sound.currentTime = 0;
+    });
+}
